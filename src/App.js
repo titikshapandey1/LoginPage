81ca630f5e9516fc8c34d91c384b9bff5b6ac316
titikshapandey1 <86707204+titikshapandey1@users.noreply.github.com>
2023-06-29 09:25:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/Logoutbutton';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+	return (
+		<>
+			<nav className="navbar bg-dark">
+				<div className="container-fluid">
+					<span className="appName">
+						React User Authentication</span>
+				</div>
+			</nav>
+			<LoginButton />
+			<LogoutButton />
+		</>
+	);
+}
+
+export default App;
+
